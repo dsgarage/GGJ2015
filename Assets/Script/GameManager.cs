0a712e001de54c4dd61f05101b1c48aee3e89f42
Yuichiro Kumagai <u16.kuma@gmail.com>
2015-01-24 18:36:49 +0900

    GameManagerからPlayerを取得可能 TimeManagerから経過時間を取得可能

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// ゲームマネージャ
+/// </summary>
+public class GameManager : SingletonMonoBehaviour<GameManager> {
+
+    #region Members
+
+    GameObject _player;
+    public GameObject Player {
+        get {
+            return _player;
+        }
+    }
+
+    #endregion
+
+    #region System Calls
+
+    void Update () {
+
+    }
+
+    #endregion
+}
