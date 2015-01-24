79b9c1133aa7864cea57121a85ce40166238f6ad
Yuichiro Kumagai <u16.kuma@gmail.com>
2015-01-24 17:41:04 +0900

    Singleton実装

---
+++ Assets/Script/Singleton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> where T : class, new() {
+	public static T Instance {
+		get {
+			if (_instance == null) {
+				_instance = new T();
+			}
+			return _instance;
+		}
+	}
+
+	static T _instance;
+}
