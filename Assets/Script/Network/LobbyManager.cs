0bb223db36ae6b74c9313e3a51041f764f5d603e
ShSug <shew.curn@gmail.com>
2015-01-24 22:14:21 +0900

    Lobby機能暫定実装

---
+++ Assets/Script/Network/LobbyManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LobbyManager : Photon.MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+
+	void OnJoinedLobby()
+	{
+		Debug.Log("JoinRandom");
+		PhotonNetwork.JoinRandomRoom();
+		Debug.Log(PhotonNetwork.connected.ToString());
+	}
+	
+	void OnPhotonRandomJoinFailed()
+	{
+		PhotonNetwork.CreateRoom(null);
+	}
+
+
+	void OnJoinedRoom()
+	{
+
+		//GUILayout.Label (PhotonNetwork.room.playerCount.ToString ());
+	}
+
+
+	void OnGUI(){
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+		if(GUILayout.Button ("JoinGame")){
+			Application.LoadLevel ("TestScene");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
