8a396c23a8d253a58a242ddbf44d2be016eddc94
krnkrs <krntrg_krnkrs@yahoo.co.jp>
2015-01-25 03:05:42 +0900

    浮力制御スクリプトを実装

---
+++ Assets/Script/FloatScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatScript : MonoBehaviour {
+
+    /// <summary>
+    /// プレイヤーオブジェクト
+    /// </summary>
+    private GameObject m_playerObject;
+    /// <summary>
+    /// オブジェクトの物理演算
+    /// </summary>
+    private Rigidbody m_rigitbody;
+    /// <summary>
+    /// 浮力
+    /// </summary>
+    private Vector3 Float;
+
+    /// <summary>
+    /// 触れた瞬間
+    /// </summary>
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.name == "Player")
+        {
+            Float = new Vector3(0, setFloatValue, 0);
+            Physics.gravity = Physics.gravity + Float;
+            //Debug.Log(m_rigitbody.useGravity);
+        }
+    }
+
+    /// <summary>
+    /// 浮力設定プロパティ
+    /// </summary>
+    public float setFloatValue { set; private get; }
+}
