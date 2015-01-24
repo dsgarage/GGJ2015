42d6f609b01b4504b65da8073904ecda09149ebe
Yuichiro Kumagai <u16.kuma@gmail.com>
2015-01-25 07:06:36 +0900

    色を徐々に変化できるように追記

--- Assets/Script/ColorScript.cs
+++ Assets/Script/ColorScript.cs
@@ -25,11 +25,28 @@ public class ColorScript : MonoBehaviour
         get { return _color; }
         set {
             _color = value;
-            MeshRenderer.material.color = Color;
+            StartCoroutine(ChangeColorAnimation());
+        }
+    }
+
+    [SerializeField]
+    float _animationTime = 0.5f;
+    public float AnimationTime {
+        get { 
+            return _animationTime;
         }
     }
 
     #endregion
+
+    #region System Calls
+
+    void Start () {
+    }
+
+    #endregion
+
+    #region Public Methods
 
     /// <summary>
     /// 色を帰る
@@ -37,5 +54,31 @@ public class ColorScript : MonoBehaviour
     public void ChangeColor ()
     {
         Color = _color;
+    }
+
+    #endregion
+
+    /// <summary>
+    /// アニメーション処理
+    /// </summary>
+    IEnumerator ChangeColorAnimation () {
+        float time = 0.0f;
+
+        Color start = MeshRenderer.material.color;
+
+        // 指定時間回す
+        while (time < AnimationTime) {
+            time += Time.deltaTime;
+
+            // 色を徐々に線形で変えていく処理
+            Color now = MeshRenderer.material.color;
+            now.r = Mathf.Lerp(start.r, Color.r, time / AnimationTime);
+            now.g = Mathf.Lerp(start.g, Color.g, time / AnimationTime);
+            now.b = Mathf.Lerp(start.b, Color.b, time / AnimationTime);
+            now.a = Mathf.Lerp(start.a, Color.a, time / AnimationTime);
+            MeshRenderer.material.color = now;
+
+            yield return null;
+        }
     }
 }
