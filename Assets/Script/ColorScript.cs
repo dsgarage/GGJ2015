10a08f058f104db1b2baee16db57948f521b2ce1
Yuichiro Kumagai <u16.kuma@gmail.com>
2015-01-25 06:26:30 +0900

    色を変更するコンポーネント

---
+++ Assets/Script/ColorScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(MeshRenderer))]
+public class ColorScript : MonoBehaviour
+{
+
+    #region Members
+
+    MeshRenderer _meshRenderer;
+
+    MeshRenderer MeshRenderer {
+        get { 
+            if (_meshRenderer == null) {
+                _meshRenderer = GetComponent<MeshRenderer> ();
+            }
+            return _meshRenderer;
+        }
+    }
+
+    [SerializeField]
+    Color _color;
+
+    public Color Color {
+        get { return _color; }
+        set {
+            _color = value;
+            MeshRenderer.material.color = Color;
+        }
+    }
+
+    #endregion
+
+    /// <summary>
+    /// 色を帰る
+    /// </summary>
+    public void ChangeColor ()
+    {
+        Color = _color;
+    }
+}
