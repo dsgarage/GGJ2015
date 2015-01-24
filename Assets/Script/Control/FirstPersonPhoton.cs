ef9a007c69a5c1eafa35dbc7c177d9365afe3271
ShSug <shew.curn@gmail.com>
2015-01-25 07:09:31 +0900

    Add CharacterController

---
+++ Assets/Script/Control/FirstPersonPhoton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (Animator))]
+[RequireComponent(typeof (CapsuleCollider))]
+[RequireComponent(typeof (Rigidbody))]
+
+
+public class FirstPersonPhoton : MonoBehaviour {
+
+	//Character Config
+	public float moveSpeed;
+	public float rotateSpeed;
+	public float backSpeed;
+	public float jumpHeight;
+
+	public bool isControllable;
+
+	private GameObject cameraObject;
+
+	private Animator anim;
+	private AnimatorStateInfo currentBaseState;
+
+	/*
+	static int idleState = Animator.StringToHash("Base Layer.Idle");
+	static int locoState = Animator.StringToHash("Base Layer.Locomotion");
+	static int jumpState = Animator.StringToHash("Base Layer.Jump");
+	static int restState = Animator.StringToHash("Base Layer.Rest");
+	 */
+
+	// Use this for initialization
+	void Start () {
+		//anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
