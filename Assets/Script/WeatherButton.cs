3764e6bbe739371d021c44eb507ec852611607e0
束田 大介 <dsgarage@gmail.com>
2015-01-25 10:34:41 +0900

    ・WeatherButtonクラス作成

---
+++ Assets/Script/WeatherButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeatherButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
