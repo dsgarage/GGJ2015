d5adc1f9143587c117f9a1fb1cdf293775bad58b
ShSug <shew.curn@gmail.com>
2015-01-25 11:24:38 +0900

    Character select

---
+++ Assets/Script/GUI/Char_Select.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Char_Select : MonoBehaviour {
+
+	public GameObject	NetworkManager;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnMouseOver ()
+	{
+		this.transform.position = new Vector3(this.transform.position.x,this.transform.position.y,4.0f);
+
+	}
+
+	void OnMouseExit() {
+		this.transform.position = new Vector3(this.transform.position.x,this.transform.position.y,5.0f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
